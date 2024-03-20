2b4a2765793ca0db732b69e1c718b7629556e408
Akhil <akhilyadav.com@gmail.com>
2024-03-20 13:18:55 +0530

    added jwt token in login response

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -1,28 +1,28 @@
-# frozen_string_literal: true
-
 class Customers::SessionsController < Devise::SessionsController
   include RackSessionsFix
   respond_to :json
 
   private
   def respond_with(current_user, _opts = {})
+    jwt_token = generate_jwt_token(current_user)
     render json: {
       status: { 
         code: 200, message: 'Logged in successfully.',
         data: {
-          customer: CustomerSerializer.new(current_user).serializable_hash[:data][:attributes]
-
+          customer: CustomerSerializer.new(current_user).serializable_hash[:data][:attributes].merge(token: jwt_token)
         }
       }
     }, status: :ok
   end
+
+  def generate_jwt_token(user)
+    payload = { sub: user.id }
+    JWT.encode(payload, Rails.application.credentials.devise_jwt_secret_key!)
+  end
+    
   def respond_to_on_destroy
-    # debugger
     if request.headers['Authorization'].present?
-
       jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
-
-      
       current_user = Customer.find(jwt_payload['sub'])
     end
     
