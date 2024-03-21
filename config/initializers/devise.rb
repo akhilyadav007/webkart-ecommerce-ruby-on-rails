4288cf7ec327613fa2f561c96a3633c689974c26
PriyankaShuklaWebkorps <priyanka.shukla@webkorps.com>
2024-03-21 15:49:51 +0530

    resolve timezone error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,7 +312,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 24.hours.from_now
+    jwt.expiration_time = 30.minutes.to_i
   
 end
   # ==> Configuration for :registerable
